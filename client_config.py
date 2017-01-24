5b1b097e23893fa462e8c4f07a8749b7ecbb7b4f
Samuel Murphy <smurphy917@gmail.com>
2017-01-24 10:58:17 -0600

    pyupdater init

---
+++ client_config.py
@@ -0,0 +1,6 @@
+class ClientConfig(object):
+    PUBLIC_KEY = 'None'
+    APP_NAME = 'CL Helper'
+    COMPANY_NAME = 'S Murphy'
+    UPDATE_URLS = ['https://github.com/smurphy917/cl_helper']
+    MAX_DOWNLOAD_RETRIES = 3
